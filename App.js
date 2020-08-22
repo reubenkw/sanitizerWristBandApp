cc9036724e23a910701b06be5e91fba080c5246d
Sara Kemper <sara.kemper@icloud.com>
2020-08-22 04:13:49 -0400

    initial dashboard

--- App.js
+++ App.js
@@ -24,55 +24,37 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import SVG, { Circle } from 'react-native-svg';
+
 const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+  return ( 
+    <View style = {styles.container}>
+      <Text style = {styles.work}>Stay Safe, Sara</Text>
+      <SVG viewBox="0 0 100 100">
+        <Circle
+        cx="90"
+        cy="60"
+        r="40"
+        strokeWidth="8"
+        stroke="green"
+        fill="none"
+      />
+      </SVG>
+    </View>
+
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  work: {
+    fontFamily: 'Lato-Regular',
+    fontSize: 20,
+  },
+
   scrollView: {
     backgroundColor: Colors.lighter,
   },
